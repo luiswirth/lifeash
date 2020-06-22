5789d77edf2517d71f87d58ceb3a9735a74a4d1c
Luis Wirth <lwirth2000@gmail.com>
2020-06-22 16:48:10 +0200

    remove refs

--- refs/java/CanonicalTreeUniverse.java
+++
@@ -1,9 +0,0 @@
-/**
- *   This Universe uses a canonicalized quadtree datastructure to
- *   hold our cells.  The only difference is how we initialize root.
- */
-public class CanonicalTreeUniverse extends TreeUniverse {
-   {
-      root = CanonicalTreeNode.create() ;
-   }
-}
--- refs/java/MemoizedTreeUniverse.java
+++
@@ -1,9 +0,0 @@
-/**
- *   This Universe uses a memoized canonicalized quadtree datastructure
- *   to hold our cells.  The only difference is how we initialize root.
- */
-public class MemoizedTreeUniverse extends TreeUniverse {
-   {
-      root = MemoizedTreeNode.create() ;
-   }
-}
